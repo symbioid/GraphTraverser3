06351a20c7ea43724acc68a1df4ba1b8076d0895
Dave Elliot <symbioid@gmail.com>
2019-07-09 00:35:33 -0500

    Add project files.

---
+++ GraphTraverser3/Graph.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GraphTraverser3 {
+    class Graph {
+
+        public int size;
+        public Node node;
+
+
+        public Graph(int s) {
+            size = s;
+            node = new Node(2);
+            
+        }
+        public void Display() {            
+                Console.WriteLine(node.val);            
+        }
+    }
+}
