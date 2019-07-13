5bdeeb7d5202f9ed473b23978b65f719ebeaabc5
symbioid <symbioid@gmail.com>
2019-07-12 22:35:50 -0500

    RECOMMIT FRESH

--- Graph.cs
+++ Graph.cs
@@ -9,6 +9,7 @@ namespace GraphTraverser3 {
 
         private int size;
         private int[,] board;
+        private Node[,] board2;
 
         public Graph(int s) {
             size = s;            
@@ -16,16 +17,29 @@ namespace GraphTraverser3 {
                 { 2, 2, 2, 1 }, 
                 { 2, 1, 1, 1 }, 
                 { 1, 2, 2, 1 },                 
-                { 2, 1, 1, 2 } };            
+                { 2, 1, 1, 2 }
+            };
 
-        }
+            board2 = new Node[size, size];  
+            
+            for (int i = 0; i < s; i++) {
+                for (int j = 0; j < s; j++) {
+                    board2[i,j] = new Node(board[i,j]);
+                }
+            }
+        }    
+        
         public void Display() {
             for (int i = 0; i < size; i++) {
                 for (int j = 0; j < size; j++) {
-                    Console.Write(board[i, j] + " ");
+                    Console.Write(board2[i, j].Val + " ");
                 }
                 Console.WriteLine();
             }
+        }
+
+        public void BuildEdges() {
+            
         }
     }
 }
