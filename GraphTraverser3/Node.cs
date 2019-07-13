30db524bbcb9e51e21972dbce96cab6d85395705
Dave Elliot <symbioid@gmail.com>
2019-07-12 21:18:13 -0500

    Replace Board of Ints with Board of Nodes (using Node Constructor);

--- GraphTraverser3/Node.cs
+++ GraphTraverser3/Node.cs
@@ -6,10 +6,16 @@ using System.Threading.Tasks;
 
 namespace GraphTraverser3 {
     class Node {
-        private int val;
+        public int Val { get; set; }
+        private int Visited;
+        private Node[] TargetNodes = new Node[4];
         
         public Node(int v) {
-            val = v;            
-        }        
+            Val = v;            
+        }   
+
+        public void AddTargetNode(int inc, Node tnode) {
+            TargetNodes[inc] = tnode;            
+        }
     }
 }
