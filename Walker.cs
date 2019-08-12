2b28828a2cf6b32b77e8e8f57eb356f1ed6ad7cf
symbioid <symbioid@gmail.com>
2019-08-12 18:22:29 -0500

    Restarting Walker Class Refactor

--- Walker.cs
+++ Walker.cs
@@ -1,37 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace GraphTraverser3 {
     class Walker {
         public Node StartNode;
         public Node CurrentNode;
         public Graph graph;
-        public List<Node> MoveList = new List<Node>();
-
-        public int MoveCount { get => MoveCount; set => MoveCount = 0; }
+        public bool IsActive { set; get; }
+        public List<Node> NodeList = new List<Node>();
+        private Queue <Node> NodeQueue = new Queue<Node>();       
         
         public Walker(Tuple<int, int> StartPosition, Node n) {
+            IsActive = true;
             StartNode = n;            
-            LogPosition(StartNode);
         }
-
-        public void Move(Node TargetNode) {
-            if (TargetNode.IsActive == true) {
-                CurrentNode.IsActive = false;
-                CurrentNode = TargetNode;
-                MoveCount++;
-            }
-            else {
-                Console.WriteLine("EOL");
-                return;
-            }
-        }
-
         public void LogPosition(Node n) {
-            MoveList.Add(n);
+            NodeList.Add(n);            
+        }
+
+        public EnqueueCurrentAndAdjacentNodes(Node n) {
+            NodeQueue.Enqueue(n);
         }
     }
 }
